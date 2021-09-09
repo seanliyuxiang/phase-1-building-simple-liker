af85c5cac1f43194bc66956dcf92787bff2a3c96
Yuxiang Li <seanliyuxiang@gmail.com>
2021-09-09 00:46:50 -0500

    commit tree f8562b76610bda12f39ccc0993d9f9a3b367932b

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
 
+// grab all the hearts, NodeList
+let heartIcons = document.querySelectorAll('.like-glyph');
+
+let modal = document.getElementById('modal');
+let modalParagraph = document.getElementById('modal-message');
+
+let callServerAndCatch = (event) => {
+  mimicServerCall()
+  .then(() => handleResponse(event))
+  .catch(error => handleError(error));
+};
+
+let handleError = (errorMessage) => {
+  modal.classList.remove('hidden');
+  modalParagraph.innerText = errorMessage;
+  setTimeout(() => {
+    modal.classList.add('hidden')
+    modalParagraph.innerText = ""
+  }, 3000);
+};
+
+let handleResponse = (event) => {
+  if (event.target.innerText === EMPTY_HEART) {
+    event.target.classList.add('activated-heart');
+    event.target.innerText = FULL_HEART;
+  } else {
+    event.target.classList.remove('activated-heart');
+    event.target.innerText = EMPTY_HEART;
+  }
+};
+
+// iterate through the hearts NodeList to add an event listener to each heart
+heartIcons.forEach(heart => {
+  heart.addEventListener('click', callServerAndCatch);
+});
 
 
 
